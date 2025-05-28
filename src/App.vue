60a6a3a266475b2a4948269c71afd83f5bbef3f7
Aleander Coopmans <alex.coopmans@web.de>
2025-05-28 14:18:26 +0200

    Added basic vue project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue'
+</script>
+
+<template>
+  <div class="p-3">
+    <header>
+      <Navbar />
+    </header>
+    <RouterView />
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+//import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <button class="btn btn-primary">lol</button>
+    <br />
+    <span class="">Hello world! </span>
+  </main>
+</template>
