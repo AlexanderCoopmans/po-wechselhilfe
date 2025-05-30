e10b5f1c9f3b0cc4450456399ad16db4d6498970
Alexander Coopmans <alexander.coopmans@stud.hn.de>
2025-05-30 14:20:40 +0200

    Added views for BWI and BWL that use the new ModuleComparator component

---
+++ src/components/ModuleComparator.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { reactive, ref } from 'vue'
+import ModuleOverview from './ModuleOverview.vue'
+
+const props = defineProps({
+  poTransitionData: {
+    type: Object,
+    required: true,
+  },
+})
+
+const poTransitionData = reactive(props.poTransitionData)
+
+function onClick() {}
+</script>
+
+<template>
+  <div class="flex flex-col items-center w-full h-full gap-3">
+    <h1 class="font-bold text-3xl"><slot name="title"></slot></h1>
+    <div class="grid grid-cols-1 gap-9">
+      <ModuleOverview
+        :modules="poTransitionData.oldModules"
+        :groups="poTransitionData.oldGroups"
+        :moduleOnClick="onClick"
+        :baseCreditPoints="poTransitionData.baseCreditPoints"
+      >
+        <template #title> Alte Prüfungsordnung </template>
+      </ModuleOverview>
+      <ModuleOverview
+        :modules="poTransitionData.newModules"
+        :groups="poTransitionData.newGroups"
+        :moduleOnClick="onClick"
+        :baseCreditPoints="poTransitionData.baseCreditPoints"
+      >
+        <template #title> Neue Prüfungsordnung </template>
+      </ModuleOverview>
+    </div>
+  </div>
+</template>
---
+++ src/views/BwiView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import ModuleComparator from '../components/ModuleComparator.vue'
+import bwiPoTransitionData from '../data/BwiPoTransition.json'
+</script>
+<template>
+  <ModuleComparator :poTransitionData="bwiPoTransitionData">
+    <template #title> Bachelor Wirtschaftsinformtik </template>
+  </ModuleComparator>
+</template>
